5cbe06cde190627a4ac02d6c50291f580a24bfba
dxjester <patbenitez@gmail.com>
2019-08-24 22:27:12 -0400

    Create dict_val_append.py

---
+++ dict_val_append.py
@@ -0,0 +1,9 @@
+# convert list of values to a dictionary utilizing index
+def compress_vector(x):
+    d = {'inds': [], 'vals': []}
+    for loc, value in enumerate(x):
+        if value != 0.0:
+            d['inds'].append()
+            d['vals'].append(val)
+
+    return d
