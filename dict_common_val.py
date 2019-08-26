ee6e43ca20fcca32a8892d19d6d002aa9537eaa0
dxjester <patbenitez@gmail.com>
2019-08-26 19:43:24 -0400

    Create dict_common_val.py

---
+++ dict_common_val.py
@@ -0,0 +1,11 @@
+# find common values in a dictionary 
+d1 == {'inds': [9, 9, 1, 9, 8, 1], 'vals': [0.28, 0.84, 0.71, 0.03, 0.04, 0.75]}
+d2 == {'inds': [0, 9, 9, 1, 3, 3, 9], 'vals': [0.26, 0.06, 0.46, 0.58, 0.42, 0.21, 0.53, 0.76]}
+
+def find_common_inds(d1, d2):
+    assert type(d1) is dict and 'inds' in d1 and 'vals' in d1
+    assert type(d2) is dict and 'inds' in d2 and 'vals' in d2
+    d1_val = d1['inds']
+    d2_val = d2['inds']
+    matching = list(set(d1_val) & set(d2_val))
+    return matching
