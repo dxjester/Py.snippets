34c3f54a1aa443795f32a7ade24a00cd0001a8bb
dxjester <patbenitez@gmail.com>
2019-08-29 10:45:14 -0400

    Create character_parse.py

---
+++ character_parse.py
@@ -0,0 +1,9 @@
+txt = "welcome to the jungle"
+txt_list = txt.split()
+print(txt_list)
+
+#character_list = list(word) for item in txt_list: 
+
+temp =[list(item) for item in txt_list]
+print(temp)
+  
