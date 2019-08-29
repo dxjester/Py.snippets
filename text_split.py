6d2efd4d1041b0b823e4650ec57e51e48d939c7d
dxjester <patbenitez@gmail.com>
2019-08-29 10:34:28 -0400

    Update text_split.py

--- text_split.py
+++ text_split.py
@@ -1,5 +1,16 @@
 txt = "welcome to the jungle"
-
 x = txt.split()
-
 print(x)
+
+
+#split on specific character
+txt = "apple#banana#cherry#orange"
+x = txt.split("#")
+print(x)
+
+# split one the first string
+txt = "apple#banana#cherry#orange"
+# setting the max parameter to 1, will return a list with 2 elements!
+x = txt.split("#", 1)
+print(x)
+# >>> ['apple', 'banana#cherry#orange']
