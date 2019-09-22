d4ab984bb03493b8479543e2cc32bc21e6386828
dxjester <patbenitez@gmail.com>
2019-09-22 11:48:40 -0400

    Create dict_zip.py

---
+++ dict_zip.py
@@ -0,0 +1,17 @@
+def decompress_vector(d, n=None):
+    # Checks the input
+    assert type(d) is dict and 'inds' in d and 'vals' in d, "Not a dictionary or missing keys"
+    assert type(d['inds']) is list and type(d['vals']) is list, "Not a list"
+    assert len(d['inds']) == len(d['vals']), "Length mismatch"
+    
+    # Determine length of the full vector
+    i_max = max(d['inds']) if d['inds'] else -1
+    if n is None:
+        n = i_max+1
+    else:
+        assert n > i_max, "Bad value for full vector length"
+
+    list_output = [0.0] * n
+    for index, values in zip(d['inds'], d['vals']):
+        list_output[index] =  list_output[index]+values
+    return list_output
