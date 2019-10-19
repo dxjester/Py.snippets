a8013c6bcced754ce3bad4fbc58edd3b9e1ff66d
dxjester <patbenitez@gmail.com>
2019-10-19 14:48:21 -0400

    added multi-dimensional array

--- numpy_snippets.py
+++ numpy_snippets.py
@@ -13,6 +13,24 @@ print(a)
 
 n = 1000000
 
+# Exercise 10.0 -------------------------------------------- #
+
+# Test time for creating i^2 calculation via list call
 L = range(n)
 print ("Running equation n^2 loop via list command for timing purposes ... \n")
 %timeit [i**2 for i in L]
+
+# Test time for creating i^2 calculation via numpy array call
+np.arange(10) # Moral equivalent to `range`
+A = np.arange(n)
+%timeit A**2
+
+# Create a multi-dimensional array 
+m_array = np.array([
+        [0,1,2,3],
+        [4,5,6,7],
+        [8,9,10,11]
+        ])
+
+print(m_array)
+
