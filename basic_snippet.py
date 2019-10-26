51ae06360ac2e6c9eab8137dfed9a04321bb2536
dxjester <patbenitez@gmail.com>
2019-10-26 14:45:06 -0400

    added find_two_away function

--- basic_snippet.py
+++ basic_snippet.py
@@ -14,3 +14,15 @@ def set_check_in_df(set_var, df, df_col_name):
     assert type(set_var) is set
     matches = df[df_col_name].apply(lambda x: x in set_var)
     return df[matches]
+
+# a string function to check if cells contain a specific string
+def find_description(str_var, df, df_col_name):
+    matches = df[df_col_name].str.contains(str_var)
+    return df[matches]
+
+# CATEGORY: dataframe, merge, column
+def find_two_away(code, df):
+    one_away = df[df['ORIGIN_AIRPORT_ID'] == code]
+    two_away = one_away.merge(df, left_on = 'DEST_AIRPORT_ID', right_on = 'ORIGIN_AIRPORT_ID')
+    return set(two_away['DEST_AIRPORT_ID_y'])
+    
