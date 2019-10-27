3606c1c3d8f4505ad5e585bf468ec20d4e2e321f
dxjester <patbenitez@gmail.com>
2019-10-27 16:55:12 -0400

    added to_frame function

--- basic_snippet.py
+++ basic_snippet.py
@@ -6,7 +6,7 @@ Created on Sat Oct 26 14:15:01 2019
 @author: patrickbenitez
 """
 
-# a function to return matching set values in a dataframe
+# CATEGORY: dataframe, pandas, column, row
 def set_check_in_df(set_var, df, df_col_name):
     '''
     a function to return matching set values located within a given dataframe
@@ -15,7 +15,7 @@ def set_check_in_df(set_var, df, df_col_name):
     matches = df[df_col_name].apply(lambda x: x in set_var)
     return df[matches]
 
-# a string function to check if cells contain a specific string
+# CATEGORY: string, char, manipulation, slicing
 def find_description(str_var, df, df_col_name):
     matches = df[df_col_name].str.contains(str_var)
     return df[matches]
@@ -25,4 +25,9 @@ def find_two_away(code, df):
     one_away = df[df['ORIGIN_AIRPORT_ID'] == code]
     two_away = one_away.merge(df, left_on = 'DEST_AIRPORT_ID', right_on = 'ORIGIN_AIRPORT_ID')
     return set(two_away['DEST_AIRPORT_ID_y'])
+
+# CATEGORY: dataframe, pandas, convert
+def calc_who_switched(Drugs):
+    who_switched = Drugs.groupby(['ID'])['Med'].unique().to_frame(name='Unique Med')
+    return who_switched
     
